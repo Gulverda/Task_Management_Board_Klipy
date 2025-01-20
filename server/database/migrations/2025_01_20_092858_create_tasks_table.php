a029c45de149aec80041e7c45de24915e93035d4
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-01-20 13:38:45 +0400

    Added Task Model and Migration

---
+++ server/database/migrations/2025_01_20_092858_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        schema::create('tasks', function (Blueprint $table){
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['To Do', 'In Progress', 'Done'])->default('To Do');
+            $table->dateTime('due_date')->nullable();
+            $table->foreignId('assigned_user_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
