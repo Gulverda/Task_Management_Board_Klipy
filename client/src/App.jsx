e429b275a495afe44b0c0da40a3c35c28328dbca
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-01-20 13:19:36 +0400

    Added Clean Front-End

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">
+        Task Management
+      </h1>
+    </div>
+  );
+}
+
+export default App;
