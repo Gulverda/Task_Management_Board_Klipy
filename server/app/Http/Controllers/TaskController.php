07200bb8bdac9aadd3802c38ea1fdf8dc5d34f27
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-01-20 13:45:19 +0400

    Added Controler

---
+++ server/app/Http/Controllers/TaskController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:To Do,In Progress,Done',
+            'due_date' => 'nullable|date',
+        ]);
+
+        return Task::create($validated);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $validated = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:To Do,In Progress,Done',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task->update($validated);
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->noContent();
+    }
+}
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,4 @@
 <?php
+use App\Http\Controllers\TaskController;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::apiResource('tasks', TaskController::class);
